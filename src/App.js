2c82afaa3f576f8d45ec05c847320409899b79f1
admin <alex.darmos@gmail.com>
2019-07-25 21:39:37 -0400

    added not found page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
-import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 import User from './components/users/User';
-
+import Home from './components/pages/Home';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
+import NotFound from './components/pages/NotFound';
 
 import './App.css';
 const App = () => {
+  //render is a required lifecycle method responsible for rendering components to the UI
+  //destructering, so we don't need this.state every time we refernce users and loading in the code
   return (
     <GithubState>
       <AlertState>
@@ -21,18 +22,10 @@ const App = () => {
 
             {/* passing props searchUsers and clearUsers from search up to its parent */}
             <Switch>
-              <Route
-                exact
-                path="/"
-                render={props => (
-                  <Fragment>
-                    <Search />
-                    <Users />
-                  </Fragment>
-                )}
-              />
+              <Route exact path="/" component={Home} />
               <Route exact path="/about" component={About} />
-              <Route exact path="./user/:login" component={User} />
+              <Route exact path="/user/:login" component={User} />
+              <Route component={NotFound} />
             </Switch>
             <Alert />
 
